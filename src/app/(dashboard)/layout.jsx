a6c6d07c3aab0e0cf1b07c90036bb9d7baca87bb
six-shot <okhuomonajayi54@gmail.com>
2025-09-16 08:49:32 +0100

    fixes

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -85,17 +85,17 @@ const Layout = ({ children }) => {
   return (
     <DashboardProvider>
       <div className="flex max-w-[1440px] mx-auto relative">
-      {/* Mobile Overlay */}
-      {isMobile && isMobileMenuOpen && (
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50 z-40 lg:hidden"
-          onClick={() => setIsMobileMenuOpen(false)}
-        />
-      )}
+        {/* Mobile Overlay */}
+        {isMobile && isMobileMenuOpen && (
+          <div
+            className="fixed inset-0 bg-[rgba(2,13,23,0.24)] backdrop-blur-[5px] z-40 lg:hidden"
+            onClick={() => setIsMobileMenuOpen(false)}
+          />
+        )}
 
-      {/* Sidebar */}
-      <div
-        className={`
+        {/* Sidebar */}
+        <div
+          className={`
           fixed h-screen bg-white z-50 transition-transform duration-300 ease-in-out
           ${
             isMobile
@@ -105,43 +105,43 @@ const Layout = ({ children }) => {
               : "translate-x-0"
           }
         `}
-        style={{ width: isMobile ? 320 : sidebarWidth }}
-      >
-        <Sidebar
-          isSidebarExpanded={isSidebarExpanded}
-          setIsSidebarExpanded={setIsSidebarExpanded}
-          isMobile={isMobile}
-          isMobileMenuOpen={isMobileMenuOpen}
-          setIsMobileMenuOpen={setIsMobileMenuOpen}
-        />
-      </div>
+          style={{ width: isMobile ? 320 : sidebarWidth }}
+        >
+          <Sidebar
+            isSidebarExpanded={isSidebarExpanded}
+            setIsSidebarExpanded={setIsSidebarExpanded}
+            isMobile={isMobile}
+            isMobileMenuOpen={isMobileMenuOpen}
+            setIsMobileMenuOpen={setIsMobileMenuOpen}
+          />
+        </div>
 
-      {/* Main Content */}
-      <div
-        className={`
+        {/* Main Content */}
+        <div
+          className={`
           flex-1 h-screen text-white w-full relative transition-all duration-300
           ${isMobile ? "ml-0" : ""}
         `}
-        style={{ marginLeft: isMobile ? 0 : sidebarWidth }}
-      >
-        <div className="sticky top-0 z-50 bg-white w-full">
-          <div className=" sm:px-6 lg:px-8">
-            <Header
-              isMobile={isMobile}
-              onMobileMenuToggle={toggleMobileMenu}
-              isMobileMenuOpen={isMobileMenuOpen}
-              title={headerContent.title}
-              description={headerContent.description}
-              showCardIcon={headerContent.showCardIcon}
-              imageSrc={headerContent.imageSrc}
-              imageAlt={headerContent.imageAlt}
-              isTransactionPage={headerContent.isTransactionPage}
-            />
+          style={{ marginLeft: isMobile ? 0 : sidebarWidth }}
+        >
+          <div className="sticky top-0 z-50 bg-white w-full">
+            <div className=" sm:px-6 lg:px-8">
+              <Header
+                isMobile={isMobile}
+                onMobileMenuToggle={toggleMobileMenu}
+                isMobileMenuOpen={isMobileMenuOpen}
+                title={headerContent.title}
+                description={headerContent.description}
+                showCardIcon={headerContent.showCardIcon}
+                imageSrc={headerContent.imageSrc}
+                imageAlt={headerContent.imageAlt}
+                isTransactionPage={headerContent.isTransactionPage}
+              />
+            </div>
           </div>
+          <div className="px-4 sm:px-6 lg:px-8 pb-8"> {children}</div>
         </div>
-        <div className="px-4 sm:px-6 lg:px-8 pb-8"> {children}</div>
       </div>
-    </div>
     </DashboardProvider>
   );
 };
